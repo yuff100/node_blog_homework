7b6415fee0357efb71a5d2d4d2c07308a25fa14c
Fisher Yu <yuff100@163.com>
2014-01-12 13:28:57 +0800

    commit

---
+++ blog/models/post.js
@@ -0,0 +1,117 @@
+var mongodb = require('./db');
+var markdown = require('markdown').markdown;
+
+function Post(name, title, post) {
+	this.name = name;
+	this.title = title;
+	this.post = post;
+}
+
+module.exports = Post;
+
+Post.prototype.save = function(callback) {
+	var date = new Date();
+console.log("Date", date.getFullYear(), date.getMonth(), date.getDate(), date.getMinutes());
+	var time = {
+		date: date,
+		year: date.getFullYear(),
+		month: date.getFullYear() + "-" + (date.getMonth() + 1),
+		day: date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate(),
+		minute: date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " " + date.getHours() + ":" + (date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes())
+	};
+console.log("time", time.year, time.month, time.day, time.minute);
+	var post = {
+		name: this.name,
+		time: time,
+		title: this.title,
+		post: this.post
+	};
+console.log("post.time", post.time.year, post.time.month, post.time.day, post.time.minute);
+
+	mongodb.open(function(err, db) {
+		if(err) {
+			return callback(err);
+		}
+
+		db.collection('posts', function(err, collection) {
+			if(err) {
+				mongodb.close();
+				return callback(err);
+			}
+
+			collection.insert(post, {
+				safe: true
+			}, function(err) {
+				mongodb.close();
+				if(err) {
+					return callback(err);
+				}
+
+				callback(null);
+			});
+		});
+	});
+}
+
+Post.getAll = function(name, callback) {
+	mongodb.open(function(err, db) {
+		if(err) {
+			return callback(err);
+		}
+
+		db.collection('posts', function(err, collection) {
+			if(err) {
+				mongodb.close();
+				return callback(err);
+			}
+
+			var query = {};
+			if(name) {
+				query.name = name;
+			}
+
+			collection.find(query).sort({
+				time: -1
+			}).toArray(function(err, docs) {
+				mongodb.close();
+				if(err) {
+					return callback(err);
+				}
+
+				docs.forEach(function(doc) {
+					doc.post = markdown.toHTML(doc.post);
+				});
+				callback(null, docs);
+			});
+		});
+	});
+}
+
+Post.getOne = function(name, day, title, callback) {
+	mongodb.open(function(err, db) {
+		if(err) {
+			return callback(err);
+		}
+
+		db.collection('posts', function(err, collection) {
+			if(err) {
+				mongodb.close();
+				return callback(err);
+			}
+
+			collection.findOne({
+				"name": name,
+				"time.day": day,
+				"title": title
+			}, function(err, doc) {
+				mongodb.close();
+				if(err) {
+					return callback(err);
+				}
+
+				doc.post = markdown.toHTML(doc.post);
+				callback(null, doc);
+			});
+		});
+	});
+};
